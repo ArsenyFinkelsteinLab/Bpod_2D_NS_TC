81e5ebc3fa25eb4ef9515b28f73b775c526894d8
TalChamilevsky <126565426+TalChamilevsky@users.noreply.github.com>
2024-07-16 17:04:22 +0300

    Delete af_2D directory

--- af_2D/manualChangeProtocol.m
+++
@@ -1,5 +0,0 @@
-function manualChangeProtocol(hObject, eventdata, input)
-global S;
-protocolType = get(hObject,'Value');
-S.GUI.ProtocolType = protocolType;
-S.ProtocolHistory(end+1,:) = [S.GUI.ProtocolType 1 0];
--- af_2D/mySet.m
+++
@@ -1,8 +0,0 @@
-function mySet(parameterName, parameterValue, varargin)
-global BpodSystem;
-p = find(cellfun(@(x) strcmp(x,parameterName),BpodSystem.GUIData.ParameterGUI.ParamNames));
-if length(varargin)==1
-	set(BpodSystem.GUIHandles.ParameterGUI.Params(p),'Value',parameterValue);
-else
-	set(BpodSystem.GUIHandles.ParameterGUI.Params(p),'String',num2str(parameterValue));
-end
